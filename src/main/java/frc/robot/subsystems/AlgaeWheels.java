23331193cb4b818a694aad8a14477c0ec061b269
Vinny_Vitali <vincentvitali4@gmail.com>
2025-01-28 16:27:39 -0600

    added variables for algae wheels

--- src/main/java/frc/robot/subsystems/AlgaeWheels.java
+++ src/main/java/frc/robot/subsystems/AlgaeWheels.java
@@ -4,11 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeWheels extends SubsystemBase {
+ 
+  private final SparkMax m_controller;
+  
+  
+  
   /** Creates a new AlgaeWheels. */
-  public AlgaeWheels() {}
+  public AlgaeWheels(SparkMax controller){
+    m_controller = controller;
+
+  }
+  /**
+   * Sets speed for motor
+   * 
+   * @param speed     speed of the motor, range -1 to 1 negative = in, positive = out
+   */
+  public void spin(double speed){
+    
+    m_controller.set(speed);
+
+  }
+
 
   @Override
   public void periodic() {
