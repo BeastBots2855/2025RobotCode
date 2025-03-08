503064f1e538962c1a98f99046ef7de6297e09f5
Isaac Patton <isaacpatton72@gmail.com>
2025-03-08 02:07:12 -0600

    maybe working vision system that only utilies one camera at the moment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,8 +10,17 @@ import com.pathplanner.lib.config.ModuleConfig;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import frc.robot.utilities.RGBColor;
@@ -170,6 +179,36 @@ public final class Constants {
 
     public static final double kUp = 3;
   }
+
+
+  public static final class VisionConstants{
+
+
+    public static final AprilTagFieldLayout kTagLayout =
+        AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+
+
+    public static final String kPlasticOrangePi = "PlasticOrangePi";
+    public static final Transform3d kRobotToPlasticTransform =
+        new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+
+    public static final String kMetalOrangePiRED = "MetalOrangePiRED";
+    public static final Transform3d kRobotToMetalREDTransform =
+        new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+    public static final String kMetalOrangePiBLUE = "MetalOrangePiBlue";
+    public static final Transform3d kRobotToMetalBLUETransform =
+      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+  }
+
+
+
+
 
  
   
