d3e6e313905450748764301f307650cb233f9b42
BeastBotDriverStation <2855BeastBot@gmail.com>
2025-03-25 18:52:00 -0500

    auto changes

--- src/main/java/frc/robot/commands/coralbox/CoralOut.java
+++ src/main/java/frc/robot/commands/coralbox/CoralOut.java
@@ -41,7 +41,7 @@ public class CoralOut extends Command {
   @Override
   public void execute() {
 
-    m_CoralBox.spin(CoralBoxConstants.kSpeed / 2);
+    m_CoralBox.spin(m_speed.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
